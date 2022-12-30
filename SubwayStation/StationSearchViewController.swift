23e6805c922e70e369ab2629089ecad1dd850470
juyeong ko <juko95@me.com>
2022-12-30 17:17:57 +0900

    feat: Implement the search screen

--- SubwayStation/StationSearchViewController.swift
+++ SubwayStation/StationSearchViewController.swift
@@ -9,17 +9,58 @@ import SnapKit
 import UIKit
 
 class StationSearchViewController: UIViewController {
+    private var numberOfCell: Int = 0
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.isHidden = true
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setNavigationItems()
+        setTableViewLayout()
+    }
+    
+    private func setNavigationItems() {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "지하철 도착정보"
         
         let searchController = UISearchController()
         searchController.searchBar.placeholder = "지하철 역을 입력해주세요."
-        searchController.obscuresBackgroundDuringPresentation = false // 차이점?
+        searchController.obscuresBackgroundDuringPresentation = false // true : 검은색 불투명
+        searchController.searchBar.delegate = self
         navigationItem.searchController = searchController
+    }
+    
+    private func setTableViewLayout() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { $0.edges.equalToSuperview() }
     }
 }
 
+extension StationSearchViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        numberOfCell = 10
+        tableView.isHidden = false
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        numberOfCell = 0
+        tableView.isHidden = true
+    }
+}
+
+extension StationSearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfCell
+    }
+    // 테이블 뷰 셀을 커스텀하지 않기 때문에 그냥 바로 cell을 초기화해서 사용
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.item)"
+        return cell
+    }
+}
