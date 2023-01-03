9dcc57a8874690a22edd7988e3b97bda344c6589
juyeong ko <juko95@me.com>
2023-01-03 15:14:57 +0900

    feat: Implementing screen

---
+++ Outstagram/Controllers/TapBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TapBarController.swift
+//  Outstagram
+//
+//  Created by juyeong koh on 2023/01/03.
+//
+
+import UIKit
+
+final class TapBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let feedViewController = UINavigationController(rootViewController: FeedViewController())
+        feedViewController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "house"),
+            selectedImage: UIImage(systemName: "house.fill")
+        )
+        
+        let profileViewController = UIViewController()
+        profileViewController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "person"),
+            selectedImage: UIImage(systemName: "person.fill")
+        )
+        
+        viewControllers = [feedViewController, profileViewController]
+    }
+}
