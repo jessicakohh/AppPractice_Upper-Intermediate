27041a75ca9f8b902e4b3e272cfdb3336265b3c1
juyeong ko <juko95@me.com>
2023-01-11 15:17:18 +0900

    feat: SearchBar

--- SearchDaumBlog/SearchBar.swift
+++ SearchDaumBlog/SearchBar.swift
@@ -8,6 +8,7 @@
 import RxCocoa
 import RxSwift
 import UIKit
+import SnapKit
 
 // 서치바에서 들어오는 텍스트를 메인 뷰가 알 수 있도록 하는 텍스트 이벤트
 // 텍스트를 입력한다고 다 보내주는게 아니라 검색 버튼을 눌렀을 때만 전달
@@ -53,19 +54,30 @@ class SearchBar: UISearchBar {
             .disposed(by: disposeBag)
         
         self.shouldLoadResult = searchButtonTapped
-            .withUnretained(self.rx.text) { $1 ?? "" }
+            .withLatestFrom(self.rx.text) { $1 ?? "" }
+            .filter { !$0.isEmpty }
+            .distinctUntilChanged()
     }
     
     private func attribute() {
-        
-
+        searchButton.setTitle("검색", for: .normal)
+        searchButton.setTitleColor(.systemBlue, for: .normal)
     }
     
     private func layout() {
+        addSubview(searchButton)
         
+        searchTextField.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(12)
+            make.trailing.equalTo(searchButton.snp.leading).offset(-12)
+            make.centerY.equalToSuperview()
+        }
+        
+        searchButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(12)
+        }
     }
-    
-    
 }
 
 
